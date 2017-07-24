c49a2e56aba8c258f0893a56215fdd94d234d9ac
Anna <annamarietursi@gmail.com>
2017-07-24 14:04:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+function todaydate()
+{    
+var today_date= new Date()    
+var myyear=today_date.getYear()    
+var mymonth=today_date.getMonth()+1    
+var mytoday=today_date.getDate()    
+document.write(myyear+"/"+mymonth+"/"+mytoday)
+}
+</script>
