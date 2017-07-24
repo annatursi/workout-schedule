8304d04269d8ef5763724f1ecbd247fdec053b96
Anna <annamarietursi@gmail.com>
2017-07-24 16:10:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function todaydate()
-{    
-var today_date= new Date()    
-var myyear=today_date.getYear()    
-var mymonth=today_date.getMonth()+1    
-var mytoday=today_date.getDate()    
-document.write(myyear+"/"+mymonth+"/"+mytoday)
-}
 function decideWeek()
 {
 var week="Weeks 1&3";
