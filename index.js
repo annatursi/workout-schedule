a868679c8e52b2b085b2d6f034bf889c457b9a56
Anna <annamarietursi@gmail.com>
2017-07-25 15:23:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function decideWeek()
   month = new Date().getMonth()+1;
   date = new Date().getDate();
   day = new Date().getDay()+1;
-  
+  var line = "Today's date is: "
 if(day>=1&&day<=10)
-{var week= "Hello";}
+{var week= line.concat(day);}
   else if(day>10&&day<=20)
   {var week="Goodbye";}
   else
