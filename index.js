f54d3f087cccad4b2ee769a6ad3ec5f77d1fae45
Anna <annamarietursi@gmail.com>
2017-07-25 15:29:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if(date>=1&&date<=10)
   else if(date>10&&date<=20)
   {var week="Goodbye";}
   else if(date>20&&date<=31&&month==7)
-  {var week="Correct";}
+  {var week="Correct!!";}
   else
   {var week="Else Statement.";}
   return week;
